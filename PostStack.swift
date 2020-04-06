99634997cf16350945185230fb45464fc333bd69
Eglantine Fonrose <eglantine.fonrose@gmail.com>
2020-04-06 15:59:54 +0200

    Plein de changements tout en bordel avec qui ne marche même plus (en gros, c'est d'la bonn' merde)

--- PostStack.swift
+++ PostStack.swift
@@ -2,33 +2,20 @@
 //  PostStack.swift
 //  fonrose-ecommerceV2
 //
-//  Created by Eglantine on 19/03/2020.
+//  Created by Eglantine on 06/04/2020.
 //  Copyright © 2020 fonrose. All rights reserved.
 //
 
 import SwiftUI
 
 struct PostStack: View {
-    
-    let pictureNamee: String
-    
     var body: some View {
-        
-        VStack {
-            //GeometryReader { geometry in
-                Image(self.pictureNamee)
-                    .resizable()
-                    .padding(.vertical, -7)
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width:UIScreen.main.bounds.width, height:UIScreen.main.bounds.height, alignment: .leading)
-                    //.clipped()
-                //}
-            } .padding(.leading, -UIScreen.main.bounds.width / 2)
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
+}
 
 struct PostStack_Previews: PreviewProvider {
     static var previews: some View {
-        PostStack(pictureNamee: "video-promo2")
+        PostStack()
     }
 }

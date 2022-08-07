d335c226e1a822a11112882b7da845c6db4287ff
Eglantine Fonrose <eglantine.fonrose@gmail.com>
2022-08-07 19:06:02 +0200

    Ajout récupération des données à un endroit + pertinant

---
+++ fonrose-ecommerceV2/Views/Location/LocationRecap.swift
@@ -0,0 +1,21 @@
+//
+//  LocationRecap.swift
+//  fonrose-ecommerceV2
+//
+//  Created by Eglantine on 07/08/2022.
+//  Copyright © 2022 fonrose. All rights reserved.
+//
+
+import SwiftUI
+
+struct LocationRecap: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LocationRecap_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationRecap()
+    }
+}

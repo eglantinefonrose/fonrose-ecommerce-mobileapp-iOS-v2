b5a801f2a00d3e93341bcbbbaef691479ac487a7
Eglantine Fonrose <eglantine.fonrose@gmail.com>
2022-10-04 12:46:21 +0200

    Suppr de isPersonChosen

---
+++ SandboxViews/findValueArrayTest.swift
@@ -0,0 +1,21 @@
+//
+//  findValueArrayTest.swift
+//  fonrose-ecommerceV2
+//
+//  Created by Eglantine on 04/10/2022.
+//  Copyright © 2022 fonrose. All rights reserved.
+//
+
+import SwiftUI
+
+struct findValueArrayTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct findValueArrayTest_Previews: PreviewProvider {
+    static var previews: some View {
+        findValueArrayTest()
+    }
+}

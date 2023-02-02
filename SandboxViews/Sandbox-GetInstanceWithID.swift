0576b4190890d6a286c0dae29585dd4a3ce26a8c
Eglantine Fonrose <eglantine.fonrose@gmail.com>
2023-02-02 19:59:23 +0100

    Fonction fetchMeasurements et fetchLocation séparées

---
+++ SandboxViews/Sandbox-GetInstanceWithID.swift
@@ -0,0 +1,21 @@
+//
+//  Sandbox-GetInstanceWithID.swift
+//  fonrose-ecommerceV2
+//
+//  Created by Eglantine Fonrose on 02/02/2023.
+//  Copyright © 2023 fonrose. All rights reserved.
+//
+
+import SwiftUI
+
+struct Sandbox_GetInstanceWithID: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Sandbox_GetInstanceWithID_Previews: PreviewProvider {
+    static var previews: some View {
+        Sandbox_GetInstanceWithID()
+    }
+}

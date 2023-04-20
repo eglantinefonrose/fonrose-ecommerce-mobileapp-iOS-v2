a0669a3efcf639af86bffef276a706ea88c3e35d
Eglantine Fonrose <eglantine.fonrose@gmail.com>
2023-04-21 01:41:02 +0200

    Ajout splash screen avec Gif temporaire (à remplacer)

---
+++ fonrose-ecommerceV2/Splash-screen.swift
@@ -0,0 +1,21 @@
+//
+//  Splash-screen.swift
+//  fonrose-ecommerceV2
+//
+//  Created by Eglantine Fonrose on 21/04/2023.
+//  Copyright © 2023 fonrose. All rights reserved.
+//
+
+import SwiftUI
+
+struct Splash_screen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Splash_screen_Previews: PreviewProvider {
+    static var previews: some View {
+        Splash_screen()
+    }
+}

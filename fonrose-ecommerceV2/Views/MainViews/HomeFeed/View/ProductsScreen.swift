f727bdfb8010e47b50edf09f9bf88937f3308b49
Eglantine Fonrose <eglantine.fonrose@gmail.com>
2023-05-12 16:16:39 +0200

    Ajout écran products

---
+++ fonrose-ecommerceV2/Views/MainViews/HomeFeed/View/ProductsScreen.swift
@@ -0,0 +1,21 @@
+//
+//  ProductsScreen.swift
+//  fonrose-ecommerceV2
+//
+//  Created by Eglantine Fonrose on 11/05/2023.
+//  Copyright © 2023 fonrose. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProductsScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProductsScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductsScreen()
+    }
+}
